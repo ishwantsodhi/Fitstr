820e783ceaf00f2c595a4ca3bb2b9869f498c3b0
Eric Feng <ericyfeng@hotmail.com>
2016-06-03 15:33:00 -0400

    updated model from second db design workshop

--- backend/db.sql
+++ backend/db.sql
@@ -1,12 +1,22 @@
 DROP TYPE if exists fitness_goal cascade;
-DROP TYPE if exists etype cascade;
+DROP TYPE if exists days_of_wk cascade;
+DROP TYPE if exists gym_type cascade;
+DROP TYPE if exists ex_type cascade;
+DROP TYPE if exists s_type cascade;
 DROP TYPE if exists experience_lvl cascade;
+DROP TYPE if exists p_type cascade;
+
 DROP TABLE if exists users cascade;
 DROP TABLE if exists user_fitness cascade;
 DROP TABLE if exists user_injury cascade;
 DROP TABLE if exists exercise cascade;
-DROP TABLE if exists equipments cascade;
+DROP TABLE If exists exercise_injured cascade;
+DROP TABLE if exists equipment cascade;
 DROP TABLE if exists fitness_program cascade;
+DROP TABLE if exists body_part cascade;
+DROP TABLE if exists data_source cascade;
+DROP TABLE if exists user_progress_weight cascade;
+DROP TABLE if exists user_progress_cardio cascade;
 
 CREATE TABLE users
 (
@@ -19,60 +29,102 @@ CREATE TABLE users
     location varchar(40)
 );
 
+CREATE TABLE equipment
+(
+    eq_id integer NOT NULL PRIMARY KEY,
+    eq_name varchar(40) NOT NULL
+);
+
 CREATE TYPE fitness_goal AS ENUM ('build_muscle', 'lose_weight', 'stay_fit');
+CREATE TYPE days_of_wk AS ENUM ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+CREATE TYPE gym_type AS ENUM ('gym', 'home');
 
 CREATE TABLE user_fitness
 (
-    u_id integer NOT NULL,
+    u_id integer NOT NULL REFERENCES users (u_id),
     current_fitness integer,
     experience integer NOT NULL,
-    fit_goal fitness_goal,
+    fit_goal fitness_goal NOT NULL,
     days_available integer,
-    busy_days varchar(3),
-    gym_type varchar(1),
-    cooks integer,
-    equipment varchar(40)
+    busy_days days_of_wk[],
+    gym_type gym_type,
+    eq_id integer[]
+);
+
+CREATE TABLE body_part
+(
+    bp_id integer NOT NULL PRIMARY KEY,
+    bp_name varchar(40) NOT NULL
 );
 
 CREATE TABLE user_injury
 (
-    u_id integer NOT NULL,
-    bodypart varchar(40)
+    u_id integer NOT NULL REFERENCES users (u_id),
+    bp_id integer NOT NULL REFERENCES body_part (bp_id)
 );
 
-CREATE TYPE etype AS ENUM ('compound', 'isolation');
+CREATE TYPE ex_type AS ENUM ('compound', 'isolation');
 
 CREATE TABLE exercise
 (
-    e_id integer NOT NULL PRIMARY KEY,
-    ename varchar(40) NOT NULL,
-    etype etype,
-    p_muscle varchar(40) NOT NULL,
-    s_muscle varchar(40) NOT NULL,
-    eq_id integer NOT NULL,
-    edemo text
+    ex_id integer NOT NULL PRIMARY KEY,
+    ex_name varchar(40) NOT NULL,
+    ex_type ex_type,
+    p_bodypart integer[] NOT NULL,
+    s_bodypart integer[] NOT NULL,
+    eq_id integer[] NOT NULL,
+    media text
 );
 
-CREATE TABLE equipments
+CREATE TABLE exercise_injured
 (
-    eq_id integer NOT NULL PRIMARY KEY,
-    eq_name varchar(40) NOT NULL,
-    eq_brand varchar(40)
+    ex_id integer NOT NULL REFERENCES exercise (ex_id),
+    bp_id integer NOT NULL REFERENCES body_part (bp_id)
+);
+
+CREATE TYPE s_type AS ENUM ('website', 'pro');
+
+CREATE TABLE data_source
+(
+    s_id integer NOT NULL PRIMARY KEY,
+    s_name varchar(40),
+    s_type s_type
 );
 
 CREATE TYPE experience_lvl AS ENUM ('beginner', 'intermediate', 'experienced', 'pro');
+CREATE TYPE p_type AS ENUM ('fullbody', 'split', 'cardio');
 
 CREATE TABLE fitness_program
 (
-    f_id integer NOT NULL PRIMARY KEY,
+    P_id integer NOT NULL PRIMARY KEY,
     p_name varchar(40) NOT NULL,
+    p_type p_type,
     author varchar(40),
     data_source varchar(40),
     fit_goal fitness_goal NOT NULL,
     exp_lvl experience_lvl NOT NULL,
     gender varchar(1) NOT NULL,
     days_per_wk integer NOT NULL,
-    eq_id integer,
+    eq_id integer[],
     bodypart_freq jsonb NOT NULL,
-    training_split jsonb NOT NULL
+    p_schedule jsonb NOT NULL
 );
+
+CREATE TABLE user_progress_weight
+(
+    u_id integer NOT NULL REFERENCES users (u_id),
+    ex_id integer NOT NULL REFERENCES exercise (ex_id),
+    weights integer NOT NULL,
+    sets integer NOT NULL,
+    reps integer NOT NULL
+);
+
+CREATE TABLE user_progress_cardio
+(
+    u_id integer NOT NULL REFERENCES users (u_id),
+    ex_id integer NOT NULL REFERENCES exercise (ex_id),
+    times integer NOT NULL,
+    speed integer NOT NULL,
+    distance integer NOT NULL
+);
+
